e7fec65c29d160283f496f31302fdf0d844cb611
tmd60 <tmd60@zips.uakron.edu>
2017-01-23 17:55:07 -0500

    Add files via upload

--- newmainADCandI2c.c
+++ newmainADCandI2c.c
@@ -12,13 +12,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-
+#define FCY 60000000ULL
+#include <libpic30.h>
 int  dSec = 0, Sec = 0; // two  global  variables
 
 //add in interupt
 
 int main(void)
 {
+    ConfigureClock();
+    I2Cinit(299);
+    ConfigureModuleADC();
+    //ChangeChannelADC(1);
     /*_T1IP = 4;//interupt setup
     TMR1 = 0;
     T1CON = 0x8010;
@@ -26,55 +31,39 @@ int main(void)
     _T1IF = 0;
     _T1IE = 1;*/
       
-    float ADC_value = 0;
-    float Voltage = 0;
-    int v = 0;
-    int weight = 0;
-    float constant = 40;//constant with voltage to weight relationship
+    double ADC_value1 = 0;
+    double ADC_value2 = 0;
+    double Voltage1 = 0;
+    double Voltage2 = 0;
+    int v1 = 0;
+    int v2 = 0;
+    //int weight = 0;
+    //float constant = 40;//constant with voltage to weight relationship
 
-    
+    ANSELAbits.ANSA1 = 1;
+    ANSELAbits.ANSA11 = 1;
     TRISAbits.TRISA0 = 0;
-    TRISAbits.TRISA1 = 1;
-   
-    I2Cinit(37);
-    ConfigureModuleADC();
-    ChangeChannelADC(1);
     
-    char c[4]= {"0000"} ;
+    char c1[4]= {"0000"};
+    char c2[4]= {"0000"};
     
     while(1)
     {
-        PORTAbits.RA0 = 1;
-               
-        ADC_value = getADC();
-        Voltage = (ADC_value*3250)/(4096);//3250 is 1000 times its value for an easier displayed value
-        v = Voltage;
-
-        //weight = constant*v;
-        
-        sprintf(c, "%04d", v);//change weight int into char array
-        msdelay(100); 
-        
-        I2CStart();
-        I2Csendbyte(0xE2);//shows write
-        msdelay(3);
-        I2Csendbyte(0x76);//clears screen
-        msdelay(3);
-        I2Csendbyte(0x77);//change decimal command
-        msdelay(3);
-        I2Csendbyte(0x01);//display decimal digit, 1 for far left and so on, 4 for colon, 5 for apostraphy     more here https://github.com/sparkfun/Serial7SegmentDisplay/wiki/Special-Commands
-        msdelay(3);
-        I2Csendbyte(c[0]);
-        msdelay(3);
-        I2Csendbyte(c[1]);
-        msdelay(3);
-        I2Csendbyte(c[2]);
-        msdelay(3);
-        I2Csendbyte(c[3]);
-        msdelay(3);
-        
-        I2CStop(); 
-        msdelay(100);
+        PORTAbits.RA0 = 1;      
+        ADC_value1 = getADC();
+        Voltage1 = (ADC_value1*3250)/(4096);//3250 is 1000 times its value for an easier displayed value
+        //v1 = Voltage1;
+        sprintf(c1, "%04f", Voltage1);//change weight int into char array
+        display(c1, 1);
+        __delay_ms(3000);
+//        PORTAbits.RA0 = 0;
+//        ChangeChannelADC(10);
+//        ADC_value2 = getADC9();
+//        Voltage2 = (ADC_value2*3250)/(4096);//3250 is 1000 times its value for an easier displayed value
+//        //v2 = Voltage2;
+//        sprintf(c2, "%04f", Voltage2);//change weight int into char array
+//        display(c2, 1);
+//        __delay_ms(3000);
     }
     return 0;
 }
