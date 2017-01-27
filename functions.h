6e4c0bafd373969963aab08b55a8a9e70a0f6d4f
tmd60 <tmd60@zips.uakron.edu>
2017-01-27 16:53:29 -0500

    Add files via upload

--- functions.h
+++ functions.h
@@ -89,7 +89,8 @@ void I2Cinit(int BRG)
     I2C1CONbits.I2CEN = 1;
 }
 
-void display(char c[], int dec){
+void display(char c[], int dec)
+{
     I2CStart();
     I2Csendbyte(0xE2);//shows write
     __delay_us(200);
@@ -169,16 +170,6 @@ float getADC(void)
     while (!AD1CON1bits.DONE);  //ADC1 Conversion Status (0 = not started/in progress, 1 = completed)
     return(ADC1BUF0);   //ADC1BUF0 = AN0 data buffer
 }
-
-//float getADC10(void)
-//{
-//    AD1CON1bits.SAMP = 1;   //ADC1 Sample enable (1 = sampling)
-//    while(!AD1CON1bits.SAMP); 
-//    AD1CON1bits.SAMP = 0;   //ADC1 Sample enable (0 = holding)
-//    while(AD1CON1bits.SAMP)
-//    while (!AD1CON1bits.DONE);  //ADC1 Conversion Status (0 = not started/in progress, 1 = completed)
-//    return(ADC1BUF9);   //ADC1BUF0 = AN0 data buffer
-//}
 
 void ConfigureClock(void)
 {
