9ef3b105e509062b2f1c5992f39525220060f908
tmd60 <tmd60@zips.uakron.edu>
2017-02-07 16:09:12 -0500

    Updates from MPLAB X IDE Take 2

--- nbproject/Package-default.bash
+++ nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/Embedded-Software.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=Embedded-Software.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=embedded-software/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/Swim_Pull_Number_1.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=Swim_Pull_Number_1.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=swimpullnumber1.x/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/embedded-software/bin
+makeDirectory ${TMPDIR}/swimpullnumber1.x/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/embedded-software.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/swimpullnumber1.x.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/embedded-software.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/swimpullnumber1.x.tar *
 checkReturnCode
 
 # Cleanup
