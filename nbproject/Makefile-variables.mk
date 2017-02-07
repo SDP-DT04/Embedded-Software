72e1952d28228029dc3df874b768dc5984380937
TMD60 <tmd60@zips.uakron.edu>
2017-02-07 15:37:34 -0500

    Carl

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -6,8 +6,8 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=Swim_Pull_Number_1.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/Swim_Pull_Number_1.X.production.hex
+CND_ARTIFACT_NAME_default=Embedded-Software.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Embedded-Software.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=swimpullnumber1.x.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/swimpullnumber1.x.tar
+CND_PACKAGE_NAME_default=embedded-software.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/embedded-software.tar
