23395840123ec14f4ca365b06997903c38c749a3
tmd60 <tmd60@zips.uakron.edu>
2017-02-08 18:45:37 -0500

    Updates to Display_new

--- display_new.c
+++ display_new.c
@@ -30,87 +30,142 @@ unsigned char commands[] = {
     0x00
 };
 
-unsigned char* cmd = commands; 
-unsigned char data = 0;
-
-enum I2C_State _state = INIT; 
-
-void display_tasks()
-{  
-    switch (_state)
-    {
-        case INIT:
-        {
-            // config
-            I2C2BRG = 299;
-            I2C2CONbits.I2CEN = 1;
-            _state = SEND_START;
-            break;
-        }
-        case SEND_START:
-        {
-            // send start
-            I2C2CONbits.SEN = 1;
-            _state = SEND_ADDR; 
-            break;
-        }
-        case SEND_ADDR:
-        {
-            if (I2C2CONbits.SEN == 0)
-            {
-                while(I2C2STATbits.TBF);
-                I2C2TRN = 0xE2;
-                _state = SEND_BYTE;
-            }
-            break;
-        }
-        case SEND_BYTE:
-        {   
-            if (I2C2STATbits.ACKSTAT == 0)
-            {
-                // we have address ack
-                data = *cmd;
-                while(I2C2STATbits.TBF);
-                I2C2TRN = *cmd;
-                _state = SEND_STOP;
-            }
-            break;
-        }
-        case SEND_STOP:
-        {
-            if (I2C2STATbits.ACKSTAT == 0)
-            {
-                I2C2CONbits.PEN = 1;
-                _state = NEXT_COMMAND; 
-            }
-            break;
-        }
-        case NEXT_COMMAND:
-        {
-            if (I2C2CONbits.PEN == 0)
-            {
-                cmd++; 
-                if (*cmd == 0x00)
-                    _state = IDLE;
-                
-                _state = SEND_START;
-            }   
-            break; 
-        }
-        case IDLE:
-        {
-            
-        }
-    }
-}
+//unsigned char* cmd = commands; 
+//unsigned char data = 0;
+//
+//enum I2C_State _state = INIT; 
+//
+//void display_tasks()
+//{  
+//    switch (_state)
+//    {
+//        case INIT:
+//        {
+//            // config
+//            I2C2BRG = 299;
+//            I2C2CONbits.I2CEN = 1;
+//            _state = SEND_START;
+//            break;
+//        }
+//        case SEND_START:
+//        {
+//            // send start
+//            I2C2CONbits.SEN = 1;
+//            _state = SEND_ADDR; 
+//            break;
+//        }
+//        case SEND_ADDR:
+//        {
+//            if (I2C2CONbits.SEN == 0)
+//            {
+//                while(I2C2STATbits.TBF);
+//                I2C2TRN = 0xE2;
+//                _state = SEND_BYTE;
+//            }
+//            break;
+//        }
+//        case SEND_BYTE:
+//        {   
+//            if (I2C2STATbits.ACKSTAT == 0)
+//            {
+//                // we have address ack
+//                data = *cmd;
+//                while(I2C2STATbits.TBF);
+//                I2C2TRN = *cmd;
+//                _state = SEND_STOP;
+//            }
+//            break;
+//        }
+//        case SEND_STOP:
+//        {
+//            if (I2C2STATbits.ACKSTAT == 0)
+//            {
+//                I2C2CONbits.PEN = 1;
+//                _state = NEXT_COMMAND; 
+//            }
+//            break;
+//        }
+//        case NEXT_COMMAND:
+//        {
+//            if (I2C2CONbits.PEN == 0)
+//            {
+//                cmd++; 
+//                if (*cmd == 0x00)
+//                    _state = IDLE;
+//                
+//                _state = SEND_START;
+//            }   
+//            break; 
+//        }
+//        case IDLE:
+//        {
+//            
+//        }
+//    }
+//}
 
 void main(void)
 {
     ConfigureClockSlow();
-    __delay_ms(1000)
-    int i = 4; 
-    while (1)
+    __delay_ms(1000);
+    I2C1BRG = 50;
+    I2C1CONbits.I2CEN = 1;
+   while (1)
     {
-        display_tasks();
+        I2C1CONbits.SEN = 1;
+        __delay_ms(100);
+        I2C1TRN = 0xE0;
+        __delay_ms(10);
+        I2C1TRN = 0x21;
+        __delay_ms(10);
+        I2C1CONbits.PEN = 1;
+        __delay_ms(100);
+        
+        I2C1CONbits.SEN = 1;
+        __delay_ms(100);
+        I2C1TRN = 0xE0;
+        __delay_ms(10);
+        I2C1TRN = 0x81;
+        __delay_ms(10);
+        I2C1CONbits.PEN = 1;
+        __delay_ms(100);
+        
+        I2C1CONbits.SEN = 1;
+        __delay_ms(100);
+        I2C1TRN = 0xE0;
+        __delay_ms(10);
+        I2C1TRN = 0xEF;
+        __delay_ms(10);
+        I2C1CONbits.PEN = 1;
+        __delay_ms(100);
+        
+        I2C1CONbits.SEN = 1;
+        __delay_ms(100);
+        I2C1TRN = 0xE0;
+        __delay_ms(10);
+        I2C1TRN = 0x00;
+        __delay_ms(10);
+        I2C1TRN = 0x7C;
+        __delay_ms(10);
+        I2C1TRN = 0x7C;
+        __delay_ms(10);
+        I2C1TRN = 0x79;
+        __delay_ms(10);
+        I2C1TRN = 0x79;
+        __delay_ms(10);
+        I2C1TRN = 0x00;
+        __delay_ms(10);
+        I2C1TRN = 0x00;
+        __delay_ms(10);
+        I2C1TRN = 0x79;
+        __delay_ms(10);
+        I2C1TRN = 0x79;
+        __delay_ms(10);
+        I2C1TRN = 0x71;
+        __delay_ms(10);
+        I2C1TRN = 0x71;
+        __delay_ms(10);
+        I2C1CONbits.PEN = 1;
+        __delay_ms(100);
     }
 }
